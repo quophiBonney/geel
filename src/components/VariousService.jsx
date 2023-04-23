d327247c1d17292297cdeca5e1f11a91dcab42be
bonney <quophiclef@gmail.com>
2023-04-23 01:28:42 -0700

    bed commit

--- src/components/VariousService.jsx
+++ src/components/VariousService.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import {Container, Row, Col} from "react-bootstrap";
 import {GiCctvCamera, GiRingingAlarm, GiWireCoil} from "react-icons/gi";
+import MainCSS from "../App.module.css";
 const VariousService = () => {
   return (
-    <Container fluid className="mt-5 mb-3 px-4">
+    <Container fluid className="mt-5 mb-3 px-4" id={MainCSS.serviceContainer}>
       <Row className="justify-content-center">
         <Col
           xs={12}
@@ -12,8 +13,11 @@ const VariousService = () => {
           lg={4}
           xl={4}
           className="mt-5 mb-3 various-service px-4 text-center"
+          data-aos="zoom-in"
+          data-aos-duration="1000"
+          data-aos-delay="100"
         >
-          <GiCctvCamera className="service-icon" />
+          <GiCctvCamera className={MainCSS.serviceIcon} />
           <h4 className="text-uppercase mt-3">CCTV Data & Systems</h4>
           <p className="text-dark">
             Fire alarm systems are essential safety devices that detect and
@@ -30,10 +34,13 @@ const VariousService = () => {
           lg={4}
           xl={4}
           className="mt-5 mb-3 various-service text-center px-4"
+          data-aos="zoom-in"
+          data-aos-duration="1000"
+          data-aos-delay="100"
         >
-          <GiRingingAlarm className="service-icon" />
+          <GiRingingAlarm className={MainCSS.serviceIcon} />
           <h4 className="text-uppercase mt-3">fire alarm systems</h4>
-          <p className="text-dark service-desc">
+          <p className="text-dark">
             As electrical engineering firm we provide security solutions for
             both residential and commercial properties. These services involve
             the installation of cameras, recording devices, and other necessary
@@ -47,10 +54,13 @@ const VariousService = () => {
           lg={4}
           xl={4}
           className="mt-5 mb-3 various-service text-center px-4"
+          data-aos="zoom-in"
+          data-aos-duration="1000"
+          data-aos-delay="100"
         >
-          <GiWireCoil className="service-icon" />
+          <GiWireCoil className={MainCSS.serviceIcon} />
           <h4 className="text-uppercase mt-3">domestic wiring</h4>
-          <p className="text-dark service-desc">
+          <p className="text-dark">
             Gilbert Electrical Engineering Ltd offers a full range of domestic
             wiring and electrical services to suit your needs – from moving a
             light switch to complete house rewires.
